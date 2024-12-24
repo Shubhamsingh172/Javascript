c624cc7c7573d89ddf70dee7bbf72eee81c396a8
Shubhamsingh172 <146461779+Shubhamsingh172@users.noreply.github.com>
2024-12-24 18:24:14 +0000

    additional operation on data is written using operator

--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -44,3 +44,41 @@ let anyNum = 33;
 let Numstring = String(anyNum);
 console.log(Numstring);
 console.log(typeof(Numstring));
+
+//--OPERATION--
+let valuee = 3
+let negvalue = -valuee;
+console.log(negvalue)
+
+// basic arithmetic operation
+// console.log(2 + 5)
+// console.log(2 - 5)
+// console.log(2 * 5)
+// console.log(2 / 5)
+// console.log(2 ** 5)
+// console.log(2 % 5)
+// console.log(5 & 6)
+
+//concatenation of two string in js
+// let str1 = "hello"
+// let str2 = "shubham singh"
+// let str3 = str1 + str2
+// console.log(str3)
+
+//string conversion confusion operation.
+// console.log("1" + 2);
+// console.log(5 + "2"); // if the starting is number type then whole is numbered type with string attached
+// console.log(1 + 2 + "2"); //if the starting is number type then whole is numbered type with string attached
+// console.log("1" + 2 + 2); //if the starting is string type then whole is converted into string type type with number type attached
+
+// console.log(3 + 4 * 5 % 3);
+// console.log(3<<2)
+
+console.log(+true) // it will give value 1
+// console.log(true+) it is invalid as it will give an error
+console.log(15 +"") // it will give same as the number which is to be added like 15 is answer here similarly if 14 provided then it is 14
+
+let counter = 100
+counter++
+console.log(counter++) // it will give 101 and convert 101 to 102 as it is a post increment
+console.log(++counter) // similarly it will give 103 as it convert 102 into 103
