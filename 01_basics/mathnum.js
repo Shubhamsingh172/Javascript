152990165e566ce291de5716521efdf3961a1df3
Shubham Shashibhushan Singh <146461779+Shubhamsingh172@users.noreply.github.com>
2024-12-27 08:52:19 +0000

    math basics and number basics go through and revised

--- 01_basics/mathnum.js
+++ 01_basics/mathnum.js
@@ -3,7 +3,7 @@
 
 // const balance = new Number(100);
 // console.log(balance);
-// console.log(balance.toString().length); // it is firstly converted into string then its length is calculating
+// console.log(balance.toString().length); // it is firstly converted into string then its length can be calculated
 // console.log(balance.toFixed(20)); // it is frequently used when we have to build e-commerce application
 
 // //precision example
@@ -19,9 +19,9 @@
 // console.log(hundreds.toLocaleString("en-IN")); // 10,00,000 like this
 
 //-------------------MATHS---------------------------
-console.log(Math);
-console.log(Math.abs(-4)); // absolute value 
-console.log(Math.abs(4));
+console.log(Math); //Object [Math] {}
+console.log(Math.abs(-4)); // absolute value i.e 4
+console.log(Math.abs(4)); //4
 console.log(Math.round(4.2)); //4
 console.log(Math.ceil(4.1)) //5
 console.log(Math.floor(4.8)) //4
