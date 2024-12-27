221557c7f118438cf8ae417ab2817381984b471e
Shubham Shashibhushan Singh <146461779+Shubhamsingh172@users.noreply.github.com>
2024-12-27 09:06:13 +0000

    stack and heap concpet and  deep copy or shallow copy concpet

--- 01_basics/stackAndheap.js
+++ 01_basics/stackAndheap.js
@@ -8,8 +8,9 @@ let anothername = myname;
 console.log(anothername)
 console.log(myname)
 
-anothername ="anshu";
+anothername = "anshu";
 console.log(anothername)
+console.log(myname);
 
 let userOne = {
     email: "user@gmail.com",
@@ -20,3 +21,24 @@ userTwo.email = "shubham12@gmail.com";
 
 console.log(userOne)
 console.log(userTwo)
+
+// Key Differences in Memory Allocation and Copying:
+// Feature	                        Stack(Primitives)	                    Heap(Reference Types)
+// 1.Allocation	                    Directly stored in the stack	        Stored in the heap, reference in the stack
+// 2.Size	                        Fixed and small	                        Dynamic and larger
+// 3.Copying Behavior	            By value(creates a copy)	            By reference(shares the same object)
+// 4.Modification Impact	        Changes affect only the copy	        Changes affect all references
+
+
+// for stack 
+let x = 5;
+let y = x;   // Copy of 'x' is created
+y = 10;      // Modifying 'y' doesn't affect 'x'
+console.log(x); // 5
+
+
+// for heap 
+let arr1 = [1, 2, 3];
+let arr2 = arr1; // Copy of reference, not the data
+arr2.push(4);
+console.log(arr1); // [1, 2, 3, 4] - both variables point to the same array
