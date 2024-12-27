72f100695e52170eb424e8c44e9c5ba3e39fcb9c
Shubham Shashibhushan Singh <146461779+Shubhamsingh172@users.noreply.github.com>
2024-12-27 06:28:39 +0000

    notes go through

--- 01_basics/comparison.js
+++ 01_basics/comparison.js
@@ -1,8 +1,8 @@
-console.log(2 < 1);
-console.log(2 >= 1);
-console.log(2 < 1);
-console.log(2 == 1);
-console.log(2 != 1);
+console.log(2 < 1); //false
+console.log(2 >= 1); //true
+console.log(2 > 1);  //true
+console.log(2 == 1); //false
+console.log(2 != 1); //true
 
 //while comparing two things like number, variables or anything remember data type must be same otherwise result may be obligatory.
 
@@ -11,14 +11,15 @@ console.log("2" > 1) // here the string 2 is directly converted into number type
 console.log("02" > 2) // as 2 is not greater than 2 therefore it returns answer as false
 
 //while performing equality check and comparison operation result can be different
-console.log(null > 0);
-console.log(null == 0);
-console.log(null >= 0); // here the null is converted into 0 therefore it returns true as there is condition greater than or equals to refer chat
+console.log("null >  0 : ",null > 0);
+console.log("null == 0 " ,null == 0);
+console.log("null >= 0 : ",null >= 0); // here the null is converted into 0 therefore it returns true as there is condition 
+// greater than or equals to refer chatgpt
 
-console.log(undefined == 0);
+console.log(undefined == 0); //undefined is declared but not defined that's why it's behaviour is not known
 console.log(undefined > 0);
 console.log(undefined < 0);
-//above all undefined operation answer is false so one need to avoid above 8 opearions.
+//above all undefined operation answer is false so one need to avoid above all 8 operations.
 
 //strict check using === triple equals to in this along with value it also check data type nor like == where automatic data is converted into 
 //another data type which returns true as result.
